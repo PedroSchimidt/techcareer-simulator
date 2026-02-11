5aeb55f8a3873df9511408689beed59a73e6da4e
PedroSchimidt <pedro.schimidtt@gmail.com>
2026-02-11 11:14:25 -0300

    rotas finalizadas

--- app.py
+++ app.py
@@ -4,6 +4,21 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = "dev" #banco de dados 
 
 #rotas 
-
 @app.route("/")
 def home():
+     return redirect(url_for("dashboard"))
+
+@app.route("dashboard")
+def dashboard():
+     return redirect(url_for("dashboard.html"))
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/ranking")
+def ranking():
+    return render_template("ranking.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
